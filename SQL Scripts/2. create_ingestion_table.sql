7077bbe22f5f1128f8b0148e611243a57008ec21
Tushar Sharma <tushar.techlab@GMAIL.COM>
2024-07-12 15:10:28 +0530

    structuring the repository
    structuring the repository

---
+++ SQL Scripts/2. create_ingestion_table.sql
@@ -0,0 +1,16 @@
+use [customer_interactions]
+go
+
+if not exists (select * from sys.tables where name = 'tbl_customer_interactions' and schema_id = schema_id('dbo'))
+begin
+    create table dbo.tbl_customer_interactions (
+        [interaction_id] int identity(1,1) primary key,
+        [user_id] int not null,
+        [product_id] int not null,
+        [action] nvarchar(50) not null,
+        [timestamp] datetime not null default getdate()
+    );
+end
+else
+print('table already exists!')
+
---
+++ SQL Scripts/3. create_fact_table.sql
@@ -0,0 +1,28 @@
+use [customer_interactions]
+go
+
+if not exists (select * from sys.tables where name = 'fact_customer_interactions' and schema_id = schema_id('dbo'))
+begin
+    create table [dbo].[fact_customer_interactions](
+	[fact_customer_interactions_id] [int] identity(1,1) not null,
+	[user_id] [int] not null,
+	[product_id] [int] not null,
+	[interaction_date] [date] not null,
+	[interaction_count] [int] not null,
+	[inserted_timestamp] [datetime] not null,
+	[updated_timestamp] [datetime] not null,
+	primary key clustered 
+	(
+		[fact_customer_interactions_id] asc
+	)with (pad_index = off, statistics_norecompute = off, ignore_dup_key = off, allow_row_locks = on, allow_page_locks = on, optimize_for_sequential_key = off) on [primary]
+	) on [primary]
+
+	alter table [dbo].[fact_customer_interactions] add  default (getdate()) for [inserted_timestamp]
+	
+	alter table [dbo].[fact_customer_interactions] add  default (getdate()) for [updated_timestamp]
+end
+else
+print('table already exists!')
+
+
+
